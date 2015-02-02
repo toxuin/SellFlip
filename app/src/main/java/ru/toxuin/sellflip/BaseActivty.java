11b8dbb05587ad830aa81a5c2f3a214e1a95184e
toxuin <otstoypuksa@ya.ru>
2015-02-02 13:23:05 -0800

    Sliding menus! Yay!

--- app/src/main/java/ru/toxuin/sellflip/BaseActivty.java
+++ app/src/main/java/ru/toxuin/sellflip/BaseActivty.java
@@ -2,16 +2,44 @@ package ru.toxuin.sellflip;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.jeremyfeinstein.slidingmenu.lib.*;
 
 public class BaseActivty extends ActionBarActivity {
+
+    private SlidingMenu leftMenu;
+    private SlidingMenu rightMenu;
+    private static final float MENU_FADE_DEGREE = 0.35f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        leftMenu = new SlidingMenu(this);
+        leftMenu.setMode(SlidingMenu.LEFT);
+        leftMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        leftMenu.setShadowDrawable(R.drawable.shadow);
+        leftMenu.setShadowWidthRes(R.dimen.menu_shadow_offset);
+        leftMenu.setFadeDegree(MENU_FADE_DEGREE);
+        leftMenu.setBehindWidthRes(R.dimen.menu_behind_offset);
+        leftMenu.setTouchmodeMarginThreshold((int) getResources().getDimension(R.dimen.menu_swipe_threshold));
+        leftMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        leftMenu.setMenu(R.layout.left_main_menu);
+
+        rightMenu = new SlidingMenu(this);
+        rightMenu.setMode(SlidingMenu.RIGHT);
+        rightMenu.setTouchModeBehind(SlidingMenu.TOUCHMODE_MARGIN);
+        rightMenu.setShadowDrawable(R.drawable.shadow_rtl);
+        rightMenu.setShadowWidthRes(R.dimen.menu_shadow_offset);
+        rightMenu.setFadeDegree(MENU_FADE_DEGREE);
+        rightMenu.setBehindWidthRes(R.dimen.menu_behind_offset);
+        rightMenu.setTouchmodeMarginThreshold((int) getResources().getDimension(R.dimen.menu_swipe_threshold));
+        rightMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        rightMenu.setMenu(R.layout.left_main_menu);
     }
 
 
@@ -35,5 +63,23 @@ public class BaseActivty extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This should fix stupid behaviour of back button & menus.
+     * Once you press Back with leftMenu open – app will quit.
+     * With this – leftMenu will close.
+     * @param keyCode what's pressed
+     * @param event the event object
+     * @return whatever.
+     */
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (leftMenu.isMenuShowing()){
+                leftMenu.toggle(true);
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
