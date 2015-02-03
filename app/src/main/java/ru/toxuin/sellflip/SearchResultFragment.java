e3166eddd16befbecd1cb9e66569f6817a0da3af
max <maks.best@hotmail.com>
2015-02-02 20:12:46 -0800

    Added useless code

--- app/src/main/java/ru/toxuin/sellflip/SearchResultFragment.java
+++ app/src/main/java/ru/toxuin/sellflip/SearchResultFragment.java
@@ -1,22 +1,34 @@
 package ru.toxuin.sellflip;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class SearchResultFragment extends Fragment {
     private View rootView;
-
+    private VideoView videoView;
     public SearchResultFragment() {} // SUBCLASSES OF FRAGMENT NEED EMPTY CONSTRUCTOR
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_results, container, false);
+        rootView = inflater.inflate(R.layout.singlead_element, container, false);
         String title = getString(R.string.search_results);
         getActivity().setTitle(title);
         // DO STUFF
+
+        // Testing video
+        videoView = (VideoView) rootView.findViewById(R.id.videoView);
+        String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.small;
+        videoView.setVideoURI(Uri.parse(path));
+        videoView.requestFocus();
+        videoView.setMediaController(new MediaController(getActivity()));
+        videoView.start();
+        //
         return rootView;
     }
 }
